0eddf7c244126f2f574a747913d5a7b9a5e241ce
agatory <agatory@gmail.com>
2020-08-07 19:05:26 +0900

    #2.6 Update Delete

--- main.go
+++ main.go
@@ -7,19 +7,15 @@ import (
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-	word := "hello"
-	definition := "Greeting"
-	err := dictionary.Add(word, definition)
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	dictionary.Search(baseWord)
+	//dictionary.Delete(baseWord)
+	word, err := dictionary.Search(baseWord)
 	if err != nil {
 		fmt.Println(err)
+	} else {
+		fmt.Println(word)
 	}
-	hello, _ := dictionary.Search("hello")
-	fmt.Println("found", word, "definition:", hello)
-	err2 := dictionary.Add("word", "gree")
-	if err2 != nil {
-		fmt.Println(err2)
-	}
-	words, _ := dictionary.Search("word")
-	fmt.Println(words)
 }
