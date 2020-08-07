60bc8becdef955d4bfd817649a3afa7b6b6e36a9
agatory <agatory@gmail.com>
2020-08-07 09:49:14 +0900

    #2.0 Account + NewAccount

--- main.go
+++ main.go
@@ -3,10 +3,10 @@ package main
 import (
 	"fmt"
 
-	"github.com/agatory/learngo/banking"
+	"github.com/agatory/learngo/accounts"
 )
 
 func main() {
-	account := banking.Account{Owner: "nicolas", Balance: 1000}
+	account := accounts.NewAccount("nico")
 	fmt.Println(account)
 }
