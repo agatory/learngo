46daac3b212ea37eafa2b97a15f822f480926b4f
agatory <agatory@gmail.com>
2020-08-07 21:57:01 +0900

    #3.7 FAST URLChecker

--- main.go
+++ main.go
@@ -6,33 +6,47 @@ import (
 	"net/http"
 )
 
+type requestResult struct {
+	url    string
+	status string
+}
+
 var errRequestFailed = errors.New("Request fail")
 
 func main() {
+	results := make(map[string]string)
+	c := make(chan requestResult)
 	urls := []string{
-		"https://www.airbnb.com/",
+		//"https://www.airbnb.com/",
 		"https://www.google.com/",
 		"https://www.amazon.com/",
-		"https://www.reddit.com/",
+		//"https://www.reddit.com/",
 		"https://www.google.com/",
 		"https://soundcloud.com/",
 		"https://www.facebook.com/",
 		"https://www.instagram.com/",
 		"https://academy.nomadcoders.co/",
+		"http://agatory.mooo.com",
 	}
 	for _, url := range urls {
-		hitURL(url)
+		go hitURL(url, c)
+
+	}
+	for i := 0; i < len(urls); i++ {
+		result := <-c
+		results[result.url] = result.status
+	}
+	for url, status := range results {
+		fmt.Println(url, status)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
+func hitURL(url string, c chan<- requestResult) {
 	resp, err := http.Get(url)
+	status := "OK"
 	if err != nil || resp.StatusCode >= 400 {
-		return errRequestFailed
+		status = "FAILED"
+	} else {
+		c <- requestResult{url: url, status: status}
 	}
-	// if(resp.StatusCode >= 400){
-
-	// }
-	return nil
 }
