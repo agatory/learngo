46e402676ecc34b7ec53a0d2a7c3c0bbcd3e18e5
agatory <agatory@gmail.com>
2020-08-07 12:03:51 +0900

    #2.5

--- main.go
+++ main.go
@@ -3,10 +3,23 @@ package main
 import (
 	"fmt"
 
-	"github.com/agatory/learngo/accounts"
+	"github.com/agatory/learngo/mydict"
 )
 
 func main() {
-	account := accounts.NewAccount("nico")
-	fmt.Println(account)
+	dictionary := mydict.Dictionary{"first": "First word"}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, _ := dictionary.Search("hello")
+	fmt.Println("found", word, "definition:", hello)
+	err2 := dictionary.Add("word", "gree")
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+	words, _ := dictionary.Search("word")
+	fmt.Println(words)
 }
